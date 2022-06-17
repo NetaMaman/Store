2f44ef43b9923e782177719d41544be0657f0cf4
danielmishan85 <danielmishan85@cs.colman.ac.il>
2022-06-17 23:47:58 +0300

    create server and connct to the mongoDB

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const DB =
+  'mongodb+srv://Danielmishan:Danielush7598@cluster0.cwwwndg.mongodb.net/store?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
