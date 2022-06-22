16eca21c03c07276dea8b065087b6741d95ff01a
Admin <Netamamann@gmail.com>
2022-06-22 23:39:00 +0300

    Added About, ContactUs and deliveries pages

---
+++ Frontend/src/pages/AboutPage.js
@@ -0,0 +1,9 @@
+import classes from './AboutPage.module.css'
+
+function AboutPage(){
+    return(
+        <div>About</div>
+    );
+}
+
+export default AboutPage;
---
+++ Frontend/src/pages/ContactUsPage.js
@@ -0,0 +1,9 @@
+import classes from './ContactUsPage.module.css'
+
+function ContactUsPage(){
+    return(
+        <div>ContactUsPage</div>
+    );
+}
+
+export default ContactUsPage;
